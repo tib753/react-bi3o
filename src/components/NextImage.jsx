c10662efadd093dd3e754a8a0c3bae20ebd9d77d
tib753 <taiebstore753@gmail.com>
2026-04-17 18:46:38 +0100

    fix: React is not defined

--- src/components/NextImage.jsx
+++ src/components/NextImage.jsx
@@ -1,7 +1,7 @@
 "use client";
 
+import React, { useEffect, useState } from "react";
 import Image from "next/image";
-import { useEffect, useState } from "react";
 import placeholder from "../../public/static/no-image-found.png";
 
 const shimmer = (w, h) => `
