58ebb473517786f283b2463194d1fd2d0dfe375b
tib753 <taiebstore753@gmail.com>
2026-06-13 22:21:16 +0100

    fix: remove custom Cache-Control headers for Vercel CDN

--- next.config.js
+++ next.config.js
@@ -86,26 +86,7 @@ const nextConfig = {
         },
       ];
     }
-    return [
-      {
-        source: "/_next/static/:path*",
-        headers: [
-          {
-            key: "Cache-Control",
-            value: "public, max-age=31536000, immutable",
-          },
-        ],
-      },
-      {
-        source: "/static/:path*",
-        headers: [
-          {
-            key: "Cache-Control",
-            value: "public, max-age=31536000, immutable",
-          },
-        ],
-      },
-    ];
+    return [];
   },
 
   compress: true,
